b49bcc2230e3e6c8908d2b3846d1b8bb6bf63378
むずでょ <muzudho1@gmail.com>
2018-08-22 22:07:01 +0900

    quittance変更☆（＾ｑ＾）

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,12 @@ fn empty_makemove_callback(_cap: &KmSyurui) {
 fn empty_unmakemove_callback(_cap: &KmSyurui) {
 }
 
-fn empty_pick_movements_callback(_max_depth: i16, _cur_depth: i16) -> HashSet<u64> {
-    HashSet::new()
+/// # Returns.
+///
+/// 1. 指し手のハッシュのセット。
+/// 2. 探索をすべて打ち切るなら真。
+fn empty_pick_movements_callback(_max_depth: i16, _cur_depth: i16) -> (HashSet<u64>, bool) {
+    (HashSet::new(), false)
 }
 
 /// 指し手の比較。
@@ -35,21 +39,23 @@ fn empty_pick_movements_callback(_max_depth: i16, _cur_depth: i16) -> HashSet<u6
 ///
 /// # Returns.
 ///
-/// 探索を打ち切るなら真。
-fn empty_compare_best_callback(_best_movement: &mut Movement, _alpha: &mut i16, _beta: i16, _movement: Movement, _child_evaluation: i16) -> bool {
-    false
+/// 1. 探索を打ち切るなら真。（ベータカット）
+/// 2. 探索をすべて打ち切るなら真。
+fn empty_compare_best_callback(_best_movement: &mut Movement, _alpha: &mut i16, _beta: i16, _movement: Movement, _child_evaluation: i16) -> (bool, bool) {
+    (false, false)
 }
-
-type CompareBestCallback = fn(best_movement: &mut Movement, alpha: &mut i16, beta: i16, movement: Movement, child_evaluation: i16) -> bool;
 
 /// 探索オブジェクト。思考開始時に作成して使う。
 pub struct AlphaBetaSearcher{
-    /// 探索を打ち切るなら真。
-    pub quittance: bool,
     pub leaf_callback: fn() -> (Movement, i16),
     pub makemove_callback: fn(&KmSyurui),
     pub unmakemove_callback: fn(&KmSyurui),
-    pub pick_movements_callback: fn(max_depth: i16, cur_depth: i16) -> HashSet<u64>,
+
+    /// # Returns.
+    ///
+    /// 1. 指し手のハッシュのセット。
+    /// 2. 探索をすべて打ち切るなら真。
+    pub pick_movements_callback: fn(max_depth: i16, cur_depth: i16) -> (HashSet<u64>, bool),
 
     /// 指し手の比較。
     ///
@@ -59,14 +65,16 @@ pub struct AlphaBetaSearcher{
     /// 4. 今回比較する指し手。
     /// 5. 今回比較する指し手の評価値。
     ///
-    /// 探索を打ち切るなら真。
-    pub compare_best_callback: CompareBestCallback, //fn(&mut Movement, &mut i16, i16, Movement, i16) -> bool,
+    /// # Returns.
+    ///
+    /// 1. 探索を打ち切るなら真。（ベータカット）
+    /// 2. 探索をすべて打ち切るなら真。
+    pub compare_best_callback: fn(&mut Movement, &mut i16, i16, Movement, i16) -> (bool, bool),
 }
 
 impl AlphaBetaSearcher{
     pub fn new()->AlphaBetaSearcher{
         AlphaBetaSearcher{
-            quittance: false,
             leaf_callback: empty_leaf_callback,
             makemove_callback: empty_makemove_callback,
             unmakemove_callback: empty_unmakemove_callback,
@@ -92,18 +100,17 @@ impl AlphaBetaSearcher{
 
 
         // 現局面の合法手を取得する。
-        let hashset_movement = (self.pick_movements_callback)(max_depth, cur_depth);
+        let (hashset_movement, quittance1) = (self.pick_movements_callback)(max_depth, cur_depth);
+        if quittance1 {
+            // 指す前に、すべての探索を打ち切る。
+            return (Movement::new(), min_alpha);
+        }
 
 
         let mut best_movement = Movement::new();
         let mut alpha = min_alpha; // ベスト評価値
         'idea: for hash_mv in hashset_movement.iter() {
             let movement = Movement::from_hash( *hash_mv );
-
-            if self.quittance {
-                // 指す前に、探索を打ち切る。
-                break;
-            }
 
             // 1手指す。
             {
@@ -117,7 +124,8 @@ impl AlphaBetaSearcher{
 
             // 比較して、一番良い手を選ぶ。
             let mut cutoff = false;
-            if (self.compare_best_callback)(&mut best_movement, &mut alpha, beta, movement, child_evaluation)
+            let (betaCutoff, quittance2) = (self.compare_best_callback)(&mut best_movement, &mut alpha, beta, movement, child_evaluation);
+            if betaCutoff
             {
                 // 手を戻したあと、探索を打ち切る。
                 cutoff = true;
@@ -128,7 +136,7 @@ impl AlphaBetaSearcher{
                 GAME_RECORD_WRAP.try_write().unwrap().unmake_movement2(self.unmakemove_callback);
             }
 
-            if cutoff || self.quittance {
+            if cutoff || quittance2 {
                 // 指した駒を戻したところで、探索を打ち切る。
                 break;
             }
